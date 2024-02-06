a42afa89295c2f387f128fe37b72cc8bf1242548
fazi1212 <fazekasdomi@gmail.com>
2024-02-06 14:35:23 +0100

    Create megoldas.sql
    minden fasza

---
+++ megoldas.sql
@@ -0,0 +1,22 @@
+-- 8.feladat
+CREATE DATABASE paralimpia
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_hungarian_ci;
+
+-- 10.feladat
+
+
+-- 11.feladat
+
+
+-- 12.feladat
+
+
+-- 13.feladat
+
+
+-- 14.feladat
+
+
+-- 15.feladat
+
